89d487a0c3343c7c60b0622b2dba3b19cebc6eaa
AwangLevy <levy.wanda.lw29@gmail.com>
2023-05-05 21:32:04 +0800

    push about

---
+++ about.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>About Us - alfa Komputer</title>
+</head>
+
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="products.php">Products</a></li>
+				<li><a href="about.php">About Us</a></li>
+				<li><a href="https://wa.wizard.id/627b7a">Contact Us</a></li>
+				<li><a href="logout.php">About Us</a></li>
+			</ul>
+		</nav>
+	</header>
+	<h1>About Us</h1>
+	<p>Toko Komputer adalah toko online yang menjual berbagai macam produk komputer, seperti laptop, PC, aksesoris, dan lain-lain. Kami memiliki pengalaman selama 10 tahun dalam menjual produk-produk komputer berkualitas dan terpercaya.</p>
+	<p>Kami berkomitmen untuk memberikan pelayanan yang terbaik kepada pelanggan kami. Kami memiliki tim yang handal dan siap membantu Anda dalam memilih produk yang sesuai dengan kebutuhan Anda. Selain itu, kami juga memberikan harga yang terjangkau dan diskon menarik bagi pelanggan setia kami.</p>
+	<p>Jangan ragu untuk menghubungi kami jika Anda memiliki pertanyaan atau membutuhkan bantuan dalam memilih produk. Kami siap membantu Anda dengan senang hati.</p>
+	<h2>Visi dan Misi</h2>
+	<ul>
+		<li>Visi: Menjadi toko online terpercaya dan terbaik dalam menjual produk-produk komputer.</li>
+		<li>Misi: Memberikan pelayanan yang terbaik kepada pelanggan dan menjual produk-produk berkualitas dengan harga yang terjangkau.</li>
+	</ul>
+</body>
+
+</html>
